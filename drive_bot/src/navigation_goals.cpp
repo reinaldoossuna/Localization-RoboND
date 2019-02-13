f26096e170d9e550dbebff1ef60e1dabddedbc20
reinaldoossuna <reinaldo.ossuna@gmail.com>
2019-02-13 20:30:28 -0300

    adding more goals

--- drive_bot/src/navigation_goals.cpp
+++ drive_bot/src/navigation_goals.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <vector>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -16,17 +17,21 @@ struct goal_pose{
 
 move_base_msgs::MoveBaseGoal create_goal(struct goal_pose);
 
-struct goal_pose GOAL1= {0.995,
-               -2.99,
-                 0.0,
-                 0.0,
-                 0.0,
-                 1.0
+vector<goal_pose> GOALs= {
+                { 0.995, -2.99, 0.0, 0.0, 0.0, 1.0},
+                {5.24, -3.65, 0.0, 0.0, 0.0, 1.0},
+                {7.25, 5.13, 0.0, 0.0, 0.0, 1.0},
+                {-3.62, 7.25, 0.0, 1.0, 1.0, 1.0},
+                {0.75, -8.88, 0.1, 0.0, 0.0, 1.0}
 };
+
+
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "navigation_goals");
+
+    ROS_INFO("NAVIGATION GOALs");
     // Spin a thread
     MoveBaseClient ac("move_base", true);
 
@@ -36,16 +41,23 @@ int main(int argc, char** argv)
 
     ROS_INFO("Connected to move_base server");
 
-    move_base_msgs::MoveBaseGoal goal = create_goal(GOAL1);
-    ROS_INFO("Sending goal");
-    ac.sendGoal(goal);
+    for(int i = 0; i < 5; i++)
+    {
+    
+      move_base_msgs::MoveBaseGoal goal = create_goal(GOALs[i]);
+      ROS_INFO("Sending goal");
+      ac.sendGoal(goal);
 
-    ac.waitForResult();
+      ac.waitForResult();
 
-    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("Excellent! Your robot has reached the goal position.");
-    else
-        ROS_INFO("The robot failed to reach the goal position");
+      if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+          ROS_INFO("Excellent! Your robot has reached the goal position.");
+      else
+          ROS_INFO("The robot failed to reach the goal position");
+      
+    
+    }
+
 
     return 0;
 }
